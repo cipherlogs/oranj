9157d566b4380c162218c1c962ea482ee939368d
Nedal <73669345+cipherlogs@users.noreply.github.com>
2024-01-11 16:52:02 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/base/",
+  base: "/oranj/",
   resolve: {
     alias: {
       "@": resolve (__dirname, "src"),
