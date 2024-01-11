7c3219e6660e01b01bdfb668efb122ff50883734
cipherlogs <73669345+0x78logs@users.noreply.github.com>
2024-01-11 16:47:42 +0100

    add base for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "orange",
   resolve: {
     alias: {
       "@": resolve (__dirname, "src"),
